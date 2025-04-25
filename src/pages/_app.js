03425fef8928239c1e68121d2d6d5d20e760c737
LukeD213 <150153401+LukeD213@users.noreply.github.com>
2025-04-25 14:22:37 +0000

    add: aula 25-04

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import Menu from "./components/menu.js";
+import Footer from "./components/footer.js";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+  <Menu />
+  <Component {...pageProps} />
+  <Footer />
+  </>
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 export default function Home(){
   return <>
-  <h1>{(1+1)*300}</h1>
   <p>Bem Vindos</p>
   </>
 }

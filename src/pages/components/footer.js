82c7d10142ee10fdd2831e8a531a6c44c5420a4f
LukeD213 <150153401+LukeD213@users.noreply.github.com>
2025-05-22 11:47:52 -0300

    Delete src directory

--- src/pages/components/footer.js
+++
@@ -1,5 +0,0 @@
-export default function Footer() {
-    return <div className="text-center fixed-bottom bg-warning-subtle -rounded-pill border boder-warning">
-    IFMS-Dourados
-    </div>
-    }
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export default function Home(){
-  return <>
-  <p>Bem Vindos</p>
-  </>
-}

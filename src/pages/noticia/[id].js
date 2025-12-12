0843e336984dced79d62942bfae516678cbcbd29
LukeD213 <150153401+LukeD213@users.noreply.github.com>
2025-12-11 23:27:55 -0400

    Update API endpoint for fetching news articles

--- src/pages/noticia/[id].js
+++ src/pages/noticia/[id].js
@@ -2,7 +2,7 @@ import { Container } from 'react-bootstrap'
 import Cards from '../components/cards'
 export async function getServerSideProps(req) {
     // Fetch data from  API notícias
-    const res = await fetch('https://apinoticias6b.vercel.app/noticias/id/' + req.query.id)
+    const res = await fetch('https://fw2-noyzv1vh9-luke-d213s-projects.vercel.app/noticias/id/' + req.query.id)
     const repo = await res.json()
     // Pass data to the page via props
     return { props: { noticia: repo } }
